368c4d514006dfd20f0a12ed8e58e589c692c773
Srivathsa <bssrivathsa2003@gmail.com>
2022-10-22 13:13:27 +0530

    release

---
+++ lib/utils/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/animation.dart';
+
+class Colo{
+  static Color bgcolor = const Color(0xff0375D7);
+  static Color whcolor = const Color(0xffffffff);
+  static Color purplecol = const Color(0xff590CF9);
+  static Color lightGreen = const Color(0xff0BFEC6);
+  static Color darkBlue = const Color(0xff1D0AF6);
+  static Color lightYellow = const Color(0xffF4DC00);
+  static Color regYellow = const Color(0xffFFC700);
+  static Color regGreen = const Color(0xff56EA87);
+  static Color red = const Color(0xffF8000F);
+}
